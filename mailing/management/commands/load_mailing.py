d0b4ba6db41c6bac69781ce0e6f21513887bb232
Oh Olya <chanfoxx@yandex.ru>
2024-01-18 15:59:26 +0300

    edit custom command

--- mailing/management/commands/load_mailing.py
+++ mailing/management/commands/load_mailing.py
@@ -1,22 +1,30 @@
 from django.core.management.base import BaseCommand
 from config.settings import MAILING_FILE
-from mailing.models import MailingSettings
 import json
 
+from mailing.models import Mailing, Message
+from users.models import User
 
-# class Command(BaseCommand):
-#     """Загружает данные из фикстуры в таблицу товаров."""
-#     def handle(self, *args, **kwargs):
-#         with open(MAILING_FILE, encoding='utf-8') as file:
-#             data = json.load(file)
-#             for product in data:
-                # category = Category.objects.get(id=product['fields']['category'])
-                # MailingSettings.objects.create(
-                #     title=product['fields']['title'],
-                #     description=product['fields']['description'],
-                #     category=category,
-                #     image=product['fields']['image'],
-                #     price=product['fields']['price'],
-                #     date_added=product['fields']['date_added'],
-                #     date_modified=product['fields']['date_modified'],
-                # )
+
+class Command(BaseCommand):
+    """ Загружает данные из фикстуры в таблицу БД рассылок. """
+
+    def handle(self, *args, **kwargs):
+        with open(MAILING_FILE, encoding='utf-8') as file:
+            data = json.load(file)
+            for mailing in data:
+                message = Message.objects.get(id=mailing['fields']['message'])
+                creator = User.objects.get(id=mailing['fields']['creator'])
+                clients_ids = mailing['fields']['clients']
+
+                new_mailing = Mailing.objects.create(
+                    start_time=mailing['fields']['start_time'],
+                    end_time=mailing['fields']['end_time'],
+                    message=message,
+                    frequency=mailing['fields']['frequency'],
+                    status=mailing['fields']['status'],
+                    creator=creator,
+                )
+
+                for client_id in clients_ids:
+                    new_mailing.clients.create(id=client_id)
