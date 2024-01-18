19cb742639bb61b55bf2d445866d0a358d0e1e7f
Oh Olya <chanfoxx@yandex.ru>
2024-01-18 16:00:26 +0300

    edit logs for send_mail

--- mailing/services.py
+++ mailing/services.py
@@ -1,41 +1,33 @@
 from smtplib import SMTPException
 from django.core.mail import send_mail
 from django.conf import settings
+
 from mailing.models import Logs
 
 
-def mailing_send(mail, client):
-    """Формирует и отправляет письмо на e-mail пользователя/пользователей."""
+def mailing_send(mail):
+    """ Формирует и отправляет письмо на e-mail пользователя/пользователей. """
     try:
-        # Пробуем отправить сообщение.
+        # Пробует отправить сообщение.
         send_mail(
             subject=mail.message.subject,
             message=mail.message.body,
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[client.email],
+            recipient_list=mail.clients.values_list('email', flat=True),
             fail_silently=False,
         )
-        try:
-            # Перезаписываем логи, если они существуют.
-            Logs.objects.get(mailing_settings=mail).delete()
-            Logs.objects.create(attempt_status=1,
-                                mail_server_response='OK', client=client,
-                                mailing_settings=mail)
-        except Exception:
-            # Записываем логи.
-            Logs.objects.create(attempt_status=1,
-                                mail_server_response='OK', client=client,
-                                mailing_settings=mail)
     except SMTPException(OSError) as e:
-        # Если возникает ошибка почтового сервиса:
-        try:
-            # Перезаписываем логи, если они существуют.
-            Logs.objects.get(mailing_settings=mail).delete()
-            Logs.objects.create(attempt_status=2,
-                                mail_server_response=e, client=client,
-                                mailing_settings=mail)
-        except Exception:
-            # Записываем логи.
-            Logs.objects.create(attempt_status=2,
-                                mail_server_response=e, client=client,
-                                mailing_settings=mail)
+        # Если возникает ошибка почтового сервиса записывает логи с ошибкой:
+        Logs.objects.create(attempt_status=2,
+                            mail_server_response=str(e),
+                            mailing_settings=mail)
+    except Exception as e:
+        # Записывает логи если возникла другая ошибка.
+        Logs.objects.create(attempt_status=2,
+                            mail_server_response=str(e),
+                            mailing_settings=mail)
+    else:
+        # Записывает успешные логи.
+        Logs.objects.create(attempt_status=1,
+                            mail_server_response='OK',
+                            mailing_settings=mail)
